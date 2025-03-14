454665751d30523e2a9114a7202633bc703564b4
Katya Weicht <katya.weicht@gmail.com>
2025-03-14 15:46:44 -0600

    Finish arrays and loops exercises

--- js_party/arrays_playground.js
+++ js_party/arrays_playground.js
@@ -2,7 +2,7 @@
       node js_party/arrays_playground.js
 */
 
-console.log("arrays_playground.js is running")
+//console.log("arrays_playground.js is running")
 
 // PART 1: How do we access values in an array?
 
@@ -10,21 +10,26 @@ var people = ['Brad', 'Dante', 'Amy', 'Louis', 'Stephanie'];
 
 // 1a. First, predict what each console.log below is going to print to the console. Then, uncomment each console.log and check your predictions. Did anything surprise you? 
 
-// console.log(people);
+console.log(people);
 
-// console.log(people[3]);
+console.log(people[3]);
 
-// console.log(people[10]);
+ console.log(people[10]);
 
 var lotteryNumbers = [12, 33, 51, 62, 74, 88];
 
 // 1b. Log the entire lotteryNumbers array to the console
-
+console.log(lotteryNumbers)
 // 1c. Log the first number to the console
-
+console.log(lotteryNumbers[0])
 // 1d. Use interpolation or concatenation (google this!) to print the following string: 
 // "The second number is...33!"
-
+console.log(`The second number is...${lotteryNumbers[1]}!`)
+var number = lotteryNumbers[1]
+var string = "The second number is..."
+var string2 = "!"
+var conact = string + number + string2
+console.log(conact)
 
 // PART 2: What type of information should arrays hold?
 
@@ -32,7 +37,12 @@ var dog = ['Cappy', 'three years old', 'loves treats', 'is really cute'];
 // 2a. This array is a bad example of an array. Why?
 
 // 2b. Create an object that represents that data in a more organized way. Try to have at least one string, number, and boolean value in your object's key:value pairs
-
+var dog = { 
+  name: 'Cappy',
+  age: 3,
+  lovesTreats: true,
+  isCute: true
+}
 
 // PART 3: Does formatting matter?
 var modOneLessonsV1 = [
@@ -57,14 +67,14 @@ var favoriteCandy = ['skittles', 'm&ms', 'snickers', 'sour patch kids'];
 favoriteCandy[1] = 'swedish fish';
 
 // 4a. What do you think the console log below will show?
-// console.log(favoriteCandy)
+console.log(favoriteCandy)
 
 // 4b. Reassign snickers to be your favorite candy. Then print the array to the console to check that it worked.
-
-favoriteCandy[10] = 'almond joy';
+favoriteCandy[0] = 'snickers'
+//favoriteCandy[10] = 'almond joy';
 
 // 4c. What do you think the console log below will show?
-// console.log(favoriteCandy)
+ console.log(favoriteCandy)
 
 
 // PART 5: Can you have an array of arrays?
@@ -75,16 +85,16 @@ var numbers = [[1, 2, 3], [4, 5], [6, [7, 8, 9], 10]];
 
 // 5z. First, predict what each console.log below is going to print to the console. Then, uncomment each console.log and check your predictions. Did anything surprise you? 
 
-// console.log(numbers[0]);
+console.log(numbers[0]);
 
-// console.log(numbers[2][1][2]);
+ console.log(numbers[2][1][2]);
 // ^^ Woah. Let's sit with this one for a minute. Break it down:
     // console.log(numbers[2]);
     // console.log(numbers[2][1]);
     // console.log(numbers[2][1][2]);
 
 // 5b. How would you access the number 10 in the numbers array?
-
+console.log(numbers[2][2])
 
 // PART 6: More nested data!
 var instructors = [
@@ -96,17 +106,17 @@ var instructors = [
 
 // 6a. First, predict what each console.log below is going to print to the console. Then, uncomment each console.log and check your predictions. Did anything surprise you? 
 
-// console.log(instructors[0]);
+ console.log(instructors[0]);
 
-// console.log(instructors[1].name);
+console.log(instructors[1].name);
 
-// console.log(instructors[2].modsTaught[0]);
+console.log(instructors[2].modsTaught[0]);
 
 // 6b. Hannah just switched to the backend program!
 
 instructors[1].program = 'BE';
 
-// console.log(instructors[1]);
+console.log(instructors[1]);
 
 var pets = [
   { 
@@ -139,18 +149,18 @@ var pets = [
 
 pets[1].favoriteTreats[0].name = 'chicken';
 
-// console.log(pets[1]);
+ console.log(pets[1]);
 
 // 6c. Print all of the pet objects to the console
-
+console.log(pets)
 // 6d. Print the string of Cap to the console
-
+console.log(pets[0]['name'])
 // 6e. Print Luna's age to the console
-
+console.log(pets[2].age)
 // 6f. Print all of Cap's favorite treats to the console (the array)
-
+console.log(pets[0].favoriteTreats)
 // 6g. Print only the string of chicken to the console
-
+console.log(pets[1].favoriteTreats[0].name)
 
 //NOTES: 
 // var array = ['dog', 8, false] //<--- GROSS
--- js_party/for_loops_playground.js
+++ js_party/for_loops_playground.js
@@ -2,14 +2,14 @@
       node js_party/for_loops_playground.js
 */
 
-console.log("for_loops_playground.js is running")
+//console.log("for_loops_playground.js is running")
 
 // Exercise 1
 
 // Uncomment the console log in the for loop to see how the value of i changes each loop.  Note that it loops 10 times but starts at 0.
 
 for (var i = 0; i < 10; i++) {
-  // console.log(i);
+   console.log(i);
 }
 
 
@@ -20,14 +20,16 @@ for (var i = 0; i < 10; i++) {
 var fruits = ['apples', 'oranges', 'bananas'];
 
 for (var i = 0; i < fruits.length; i++) {
-  // console.log(`I have some ${fruits[i]}`);
+   console.log(`I have some ${fruits[i]}`);
 }
 
 
 // Exercise 3
 
 // Write a simple for loop that will loop 4 times and print "I think I can." to the console.  (No array for this exercise, just looping 4 times.)
-
+for (var i = 0; i < 4; i++){
+  console.log("I think I can")
+}
 
 
 // Exercise 4
@@ -35,6 +37,9 @@ for (var i = 0; i < fruits.length; i++) {
 var mamboTeam = ['Angela', 'Pamela', 'Sandra', 'Rita']
 
 // Write a for loop that console logs each member of the mambo team one at a time.
+for(var i = 0; i < mamboTeam.length; i ++){
+  console.log(mamboTeam[i])
+}
 // Expected result:
   //Angela
   //Pamela
@@ -45,7 +50,10 @@ var mamboTeam = ['Angela', 'Pamela', 'Sandra', 'Rita']
 
 var nums = [10, 30, 20, 40, 60, 20]
 var sum = 0
-
+for (var i = 0; i < nums.length; i ++){
+  sum += nums[i]
+}
+console.log(sum)
 // Use a for loop to add up all the numbers in the nums array.  You'll want to look at each number one at a time and add it to the sum variable. Then, after the for loop, console log sum to confirm the expect result.
 // Expected result: 180
 
@@ -77,12 +85,16 @@ var pets = [
 // Tilly
 // Sodie
 // Pumpernickel
-
+for (var i = 0; i < pets.length; i ++){
+  console.log(pets[i].name)
+}
 
 
 // 6b. Using a for loop, print the following sentences to the console (in this order!):
 // Leta's cat, Tilly, loves cheese.
 // Amy's dog, Sodie, loves milkbones.
 // Eric's cat, Pumpernickel, loves kibble.
-
+for (var i = 0; i < pets.length; i ++){
+  console.log(`${pets[i].human}'s ${pets[i].type}, ${pets[i].name}, loves ${pets[i].favoriteTreat}.`)
+}
 
