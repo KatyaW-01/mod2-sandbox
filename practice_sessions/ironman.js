d91c4141711482a64c938d3f578df466ec15eee3
Katya Weicht <katya.weicht@gmail.com>
2025-04-12 10:17:51 -0600

    Practice Problems

--- practice_sessions/ironman.js
+++ practice_sessions/ironman.js
@@ -4,7 +4,7 @@ Ironman - JS Iteration Station practice
 To run the code in this file, run the command:
   node practice_sessions/ironman.js
 */ 
-console.log("ironman.js is running")
+console.log("ironman.js is running\n")
 
 
 const ironmanData = {
@@ -62,7 +62,20 @@ As you work:
 */
 
 
+function calculateAverage(event){
+  let eventArray = ironmanData[event]
 
+  let timeSum = eventArray.reduce((acc,athlete)=>{
+    acc += athlete.time
+    return acc
+  },0)
+  
+  let averageTime = timeSum/eventArray.length
+  return averageTime
+}
+
+console.log(calculateAverage('swim'))
+console.log(calculateAverage('run'))
 
 
 /* Level 2
@@ -84,3 +97,99 @@ As you work:
 - At any point you should be able to explain what you're trying to do with any given line of code you're writing.
 */
 
+function calculateAverageByAge(event,age){
+  let eventArray = ironmanData[event]
+
+  let filteredArray = eventArray.filter((athlete) => athlete.age === age)
+
+  let timeSum = filteredArray.reduce((acc,athlete)=>{
+    acc += athlete.time
+    return acc
+  },0)
+
+  let averageTime = timeSum/filteredArray.length
+  return averageTime
+
+}
+
+console.log(calculateAverageByAge('swim', 29))
+console.log(calculateAverageByAge('swim', 34))
+console.log(calculateAverageByAge('swim', 41))
+
+/* Level 3
+
+Write a function that takes in an event and returns the name of the athlete with the fastest time in that event.
+
+findTopPerformer('swim') --> "Sarah Lee"
+findTopPerformer('run') --> "Nina Gupta"
+
+Start with:
+- What does it mean to be the “top performer” in this context?
+- What comparisons do I need to make during the iteration?
+- What information do I need to keep track of as I go?
+
+As you work:
+- What do you need to compare across athletes to determine who the top performer is?
+- What might you need to keep track of as you go?
+- There are a couple of ways you could approach this — think about how you'd solve this problem with paper and a highlighter first.
+- As always: console log early, console log often.
+- At any point you should be able to explain what you're trying to do with any given line of code you're writing.
+*/
+
+function findTopPerformer(event){
+  let eventArray = ironmanData[event]
+
+  let timeArray = eventArray.reduce((acc,athlete)=>{
+    acc.push(athlete.time)
+    return acc
+  },[])
+
+  let minTime = Math.min(...timeArray)
+
+  let fastestAthlete = eventArray.filter((athlete)=> athlete.time === minTime)
+
+  return fastestAthlete[0].name
+
+}
+
+console.log(findTopPerformer('swim'))
+console.log(findTopPerformer('run'))
+
+/* Level 4
+
+Write a function that takes in an event and returns an object where each key is a triclub name and each value is all the times for athletes from that triclub in that event.
+
+calculateAverageByClub('swim'); --> 
+{
+  'Endurance Warriors': [ 55 ],
+  'Peak Performers': [ 50, 60 ],
+  'Speed Demons': [ 62 ],
+  'Iron Eagles': [ 58, 54, 52 ],
+  'Aqua Flyers': [ 57 ]
+}
+
+Start with:
+- What’s the first thing I need to do to access just the data for that event?
+- How do I group entries by triclub?
+
+As you work:
+- How can I determine the triclub for each athlete?
+- What might be a good way to group athletes by their triclub?
+- What kind of structure do I need to build up (object with arrays as values)?
+- What should I do when I come across a triclub I haven’t seen yet?
+- Console log often: What does my result object look like after each iteration?
+- At any point you should be able to explain what you're trying to do with any given line of code you're writing.
+*/
+
+function calculateAverageByClub(event){
+  let eventArray = ironmanData[event]
+
+
+  // let eventHash = eventArray.reduce((acc,athlete)=>{
+  //   acc[athlete.triclub] = 
+  //   return acc
+  // },{})
+  
+}
+
+console.log(calculateAverageByClub('swim'))
